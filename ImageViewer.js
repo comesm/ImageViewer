652df85ec389631099cae0d486456b54cf419bba
Michael <comesm@gmail.com>
2017-03-21 14:31:11 -0700

    setting variables to listen for drag and target

--- ImageViewer.js
+++ ImageViewer.js
@@ -17,14 +17,29 @@ let images = ['pictures/1.png','pictures/10.png','pictures/11.png',
 
 })()
 
-//listen for a drag event on DOM event
+let dragged;
+//listen for start of drag event on DOM event
  document.addEventListener('dragstart',
     (event) => {
+      dragged = event.target;
       console.log(event.target);
     });
 
- document.addEventListener('dragenter',
+//when we drag over an event prevent
+//default to allow a 'drop'
+document.addEventListener('dragover',
     (event) => {
+      event.preventDefault();
+    });
 
-      console.log(event.target);
+
+//listen for drop event
+ document.addEventListener('drop',
+    (event) => {
+      console.log('37 dragged ', dragged);
+      console.log('38 event.target: ', event.target);
+      let path = event.target.src.split('ImageViewer/')
+      //console.log(path[path.length -1]);
+
+
     });
