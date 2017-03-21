fd0e45c4027d061baa4f9cc6ac2c828c9972cf1b
Michael <comesm@gmail.com>
2017-03-21 14:54:52 -0700

    add insertion functionality

--- ImageViewer.js
+++ ImageViewer.js
@@ -6,18 +6,22 @@ let images = ['pictures/1.png','pictures/10.png','pictures/11.png',
 ];
 
 (() => {
-  console.log('9');
+  renderImgs();
 
+})()
+
+let dragged;
+
+function renderImgs() {
+  document.getElementById('mainDisplay').innerHTML = '';
   images.forEach((image) => {
     let currentImage = document.createElement('img');
     currentImage.src = image;
     currentImage.class = 'photo';
     document.getElementById('mainDisplay').append(currentImage);
   })
+}
 
-})()
-
-let dragged;
 //listen for start of drag event on DOM event
  document.addEventListener('dragstart',
     (event) => {
@@ -39,7 +43,10 @@ document.addEventListener('dragover',
       console.log('37 dragged ', dragged);
       console.log('38 event.target: ', event.target);
       let path = event.target.src.split('ImageViewer/')
-      //console.log(path[path.length -1]);
-
-
+      //images.indexOf(path);
+      let arrayElem = path[path.length - 1];
+      let idx = images.indexOf(arrayElem);
+      images.splice(idx, 0, dragged.src);
+      //images.splice(idx, 1);
+      renderImgs();
     });
